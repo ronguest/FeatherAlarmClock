d47a5d29a523bf4ee8b96a9d3a9462566d737f63
roadfun <ronaldg@PowerHouse.local>
2017-08-27 07:26:29 -0500

    Revert "working, hardware issue"
    This reverts commit 8187635a0bcb106dfdfcb88f612d370a9f324b92.

--- src/FeatherClock.cpp
+++ src/FeatherClock.cpp
@@ -94,8 +94,8 @@ void setup() {
     musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
   #endif
 
-  Serial.println("*** Just play a song");
-  musicPlayer.startPlayingFile(alarmSong);
+  //Serial.println("*** Just play a song");
+  //musicPlayer.startPlayingFile(alarmSong);
   clockDisplay.print(6, DEC);
   clockDisplay.writeDisplay();
 }
